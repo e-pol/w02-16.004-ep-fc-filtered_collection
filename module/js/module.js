428b61b2bb994b2ab0228811633f6bda682a59cc
e.pol <eugene.poletski@gmail.com>
2016-04-14 16:55:55 +0300

    Added item page html and css

--- module/js/module.js
+++ module/js/module.js
@@ -43,6 +43,105 @@ $(function(){
             bedrooms: 1,
             technology: 'Лафет',
             imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16004',
+            title: 'Проект ПБ-16.001',
+            type: 'house',
+            area: 200,
+            floors: 2,
+            realized: 'В процессе',
+            bedrooms: 3,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16005',
+            title: 'Проект ПБ-16.002',
+            type: 'house',
+            area: 150,
+            floors: 2,
+            realized: '2016-Iкв',
+            bedrooms: 2,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16006',
+            title: 'Проект ПБ-16.003',
+            type: 'bath',
+            area: 100,
+            floors: 1,
+            realized: '2016-IIкв',
+            bedrooms: 1,
+            technology: 'Лафет',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16007',
+            title: 'Проект ПБ-16.001',
+            type: 'house',
+            area: 200,
+            floors: 2,
+            realized: 'В процессе',
+            bedrooms: 3,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16008',
+            title: 'Проект ПБ-16.002',
+            type: 'house',
+            area: 150,
+            floors: 2,
+            realized: '2016-Iкв',
+            bedrooms: 2,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16009',
+            title: 'Проект ПБ-16.003',
+            type: 'bath',
+            area: 100,
+            floors: 1,
+            realized: '2016-IIкв',
+            bedrooms: 1,
+            technology: 'Лафет',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16010',
+            title: 'Проект ПБ-16.001',
+            type: 'house',
+            area: 200,
+            floors: 2,
+            realized: 'В процессе',
+            bedrooms: 3,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16011',
+            title: 'Проект ПБ-16.002',
+            type: 'house',
+            area: 150,
+            floors: 2,
+            realized: '2016-Iкв',
+            bedrooms: 2,
+            technology: 'Проф. брус',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
+        },
+        {
+            id: '16012',
+            title: 'Проект ПБ-16.003',
+            type: 'bath',
+            area: 100,
+            floors: 1,
+            realized: '2016-IIкв',
+            bedrooms: 1,
+            technology: 'Лафет',
+            imageSrc: 'data/project_pb-16_001/project_pb-16_001.jpg'
         }
     ];
 
